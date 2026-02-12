cbb50554c904f6edf586c43ff84e729171ca72ee
Nicolas Pedrazzi <nicolas.pedrazzi@hotmail.com>
2026-02-12 17:46:18 -0500

    added post entry in database

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -35,3 +35,19 @@ export const feedFollows = pgTable("feed_follows", {
 }, (table) => ({
   uniqueUserFeed: unique().on(table.userId, table.feedId),
 }));
+
+export const posts = pgTable("posts", {
+  id: uuid("id").primaryKey().defaultRandom().notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at")
+    .notNull()
+    .defaultNow()
+    .$onUpdate(() => new Date()),
+  title: text("title").notNull(),
+  url: text("url").notNull().unique(),
+  description: text("description").notNull(),
+  publishedAt: timestamp("published_at").notNull(),
+  feedId: uuid("feed_id")
+    .notNull()
+    .references(() => feeds.id, { onDelete: "cascade" }),
+});
